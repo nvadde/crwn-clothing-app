e46a01006593a46abcc2b8f6aca8f0791e61bb2a
Nathaniel <vadde.nathaniel@gmail.com>
2020-06-30 01:22:32 +0530

    added cart dropdown and toggle logic

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,22 +1,43 @@
 import React from "react";
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { auth } from '../../firebase/firebase.utils';
 import { ReactComponent as Logo } from "../../assets/crown.svg";
+
+import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 import "./header.styles.scss";
 
-const Header = () => (
-    <div className="header">
-        <Link className="logo-container" to="/">
-            <Logo className="logo" />
+const Header = ({ currentUser, hidden }) => (
+  <div className='header'>
+    <Link className='logo-container' to='/'>
+      <Logo className='logo' />
+    </Link>
+    <div className='options'>
+      <Link className='option' to='/shop'>
+        SHOP
         </Link>
-        <div className="options">
-            <Link className="option" to="/shop">
-                SHOP
+      <Link className='option' to='/shop'>
+        CONTACT
         </Link>
-            <Link className="option" to="/shop">
-                CONTACT
-        </Link>
+      {currentUser ? (
+        <div className='option' onClick={() => auth.signOut()}>
+          SIGN OUT
         </div>
+      ) : (
+          <Link className='option' to='/signin'>
+            SIGN IN
+          </Link>
+        )}
+      <CartIcon />
     </div>
+    { hidden ? null : <CartDropdown /> }    
+  </div>
 );
 
-export default Header;
+const mapStateToProps = ({user: { currentUser }, cart: { hidden }}) => ({
+  currentUser,
+  hidden
+});
+
+export default connect(mapStateToProps)(Header);
